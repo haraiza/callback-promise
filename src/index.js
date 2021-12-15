698232fe7d3e26354739e4ac12f89f5dbae90891
humberto-desk <humberto_araiza@outlook.com>
2021-12-14 22:16:37 -0800

    primer

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import { buscarHeroe } from './js/callbacks'
+import './styles.css';
+
+const heroeId1 = 'capi';
+const heroeId2 = 'iron';
+
+// Estandar del callback
+//* Los callback siempre envian 2 variables como resultado. 
+//* Si no encuentra el heroe enviara el err con el mensaje
+//* si lo encuentra entonces dejara nulo el err y regresara el heroe
+buscarHeroe(heroeId, (err, heroe) => {
+
+    if (err) {
+        console.error(err);
+    }
+    else {
+        console.info(heroe);
+    }
+
+});
+
+console.log('Fin del programa');
+
+
---
+++ src/js/callbacks.js
@@ -0,0 +1,36 @@
+const heroes = {
+    capi: {
+        nombre: 'Capitan America',
+        poder: 'Aguantar inyeccionoes sin morir'
+    },
+    iron: {
+        nombre: 'Ironman',
+        poder: 'Absurda cantidad de dinero'
+    },
+    spider: {
+        nombre: 'Spiderman',
+        poder: 'La mejor reaccion alergica a la picadura de arañas'
+    }
+}
+
+
+//! Funcionamiento de Callback 
+
+//* Primero: recibe id y lo guarda en 'heroe'
+//* Segundo, el 'callback' del archivo en callback.js le envia el heroe al 
+//* segundo paramentro en index.js (heroe)
+//* Tercero, se ejecuta desde index.js el console.log(heroe)
+export const buscarHeroe = (id, callback) => {
+    console.log('Estoy en "callback.js"');
+    const heroe = heroes[id];
+
+    if (heroe) {
+        callback(null, heroe);
+    }
+    else {
+        callback(`No existe un heroe con el id ${id}`, '');
+    }
+};
+
+
+
